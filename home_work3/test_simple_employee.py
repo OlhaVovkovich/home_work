a9e5592916d369aded9d24b96a150b612493dba4
OlhaVovkovich <Vovkovicho@outlook.com>
2021-01-13 22:38:09 +0200

    Improved test simpe employee tests

--- home_work3/test_simple_employee.py
+++ home_work3/test_simple_employee.py
@@ -1,6 +1,7 @@
 import requests
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized_class
 
 
 class Employee:
@@ -32,38 +33,56 @@ class Employee:
             return 'Bad Response!'
 
 
-person1 = Employee('Seok-Jin', 'Kim', 100000)
-person2 = Employee('Taehyung', 'Kim', 200000)
-
-
+@parameterized_class(('first', 'last', 'pay', 'email', 'fullname', 'up', 'res_ok', 'res_text', 'month', 'url'), [
+    (
+        'Seok-Jin',
+        'Kim',
+        100000,
+        'Seok-Jin.Kim@email.com',
+        'Seok-Jin Kim',
+        105000,
+        True,
+        'Done',
+        'June',
+        'http://company.com/Kim/June'
+    ),
+    (
+        'Taehyung',
+        'Kim',
+        200000,
+        'Taehyung.Kim@email.com',
+        'Taehyung Kim',
+        210000,
+        False,
+        'Bad Response!',
+        'July',
+        'http://company.com/Kim/July'
+    )
+])
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.person = Employee(cls.first, cls.last, cls.pay)
+
     def test_email(self):
-        self.assertEqual(person1.email, 'Seok-Jin.Kim@email.com')
-        self.assertEqual(person2.email, 'Taehyung.Kim@email.com')
+        self.assertEqual(self.person.email, self.email)
 
     def test_fullname(self):
-        person1.first = 'Nam-joon'
-        person2.last = 'Vi'
-
-        self.assertIsNot(person1.fullname, 'Seok-Jin Kim')
-        self.assertEqual(person2.fullname, 'Taehyung Vi')
+        self.assertEqual(self.person.fullname, self.fullname)
 
     def test_apply_raise(self):
-        person1.apply_raise()
-        self.assertEqual(person1.pay, 105000)
-        person2.apply_raise()
-        self.assertEqual(person2.pay, 210000)
+        self.person.apply_raise()
+        self.assertEqual(self.person.pay, self.up)
 
     def test_monthly_schedule(self):
         with patch('requests.get') as mock:
-            mock.return_value.ok = False
-            mock.return_value.text = 'Done'
+            mock.return_value.ok = self.res_ok
+            mock.return_value.text = self.res_text
 
-            person = Employee('Ji-min', 'Park', 1000000)
-            schedule = person.monthly_schedule('June')
-            mock.assert_called_with("http://company.com/Park/June")
-            self.assertEqual(schedule, 'Bad Response!')
+            schedule = self.person.monthly_schedule(self.month)
+            mock.assert_called_with(self.url)
+            self.assertEqual(schedule, self.res_text)
 
 
 if __name__ == '__main__':
